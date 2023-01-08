773ec6ac58a2296426824d8f444c7e00ff987afc
Damian Poddebniak <poddebniak@mailbox.org>
2023-01-08 19:10:33 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Annotation {
+    #[serde(rename = "@context")]
+    pub context: String,
+    pub r#type: String,
+    pub body: Vec<Body>,
+    pub target: Target,
+    pub id: String,
+    pub meta: Option<GitHubStatus>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(tag = "type")]
+pub enum Body {
+    TextualBody { purpose: String, value: String },
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Target {
+    selector: Vec<Selector>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(tag = "type")]
+pub enum Selector {
+    TextQuoteSelector { exact: String },
+    TextPositionSelector { start: usize, end: usize },
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum GitHubStatus {
+    Open,
+    Closed,
+}
+
+impl Annotation {
+    pub fn title(&self) -> String {
+        if let Some(comment) = self.comment() {
+            comment
+        } else if let Some(quote) = self.quote() {
+            quote
+        } else {
+            "<error: no title>".to_string()
+        }
+    }
+
+    pub fn quote(&self) -> Option<String> {
+        let mut quote = None;
+
+        for selector in self.target.selector.iter() {
+            match selector {
+                Selector::TextQuoteSelector { exact } => {
+                    let my_quote: String = exact
+                        .lines()
+                        .map(|line| format!("{} ", line.trim()))
+                        .collect();
+
+                    quote = Some(my_quote.trim().to_string())
+                }
+                _ => {}
+            }
+        }
+
+        quote
+    }
+
+    pub fn comment(&self) -> Option<String> {
+        let mut comment = None;
+
+        for body in self.body.iter() {
+            match body {
+                Body::TextualBody { purpose, value } if purpose == "commenting" => {
+                    comment = Some(value.into())
+                }
+                _ => {}
+            }
+        }
+
+        comment
+    }
+
+    pub fn text_quote_selector(&self) -> Option<String> {
+        match self.target.selector.first()? {
+            Selector::TextQuoteSelector { exact } => {
+                let quote: String = exact
+                    .lines()
+                    .map(|line| format!("    {}\r\n", line.trim()))
+                    .collect();
+                Some(quote.trim().to_string())
+            }
+            _ => None,
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+pub fn extract_annotation(text: &str) -> Option<serde_json::Result<Annotation>> {
+    if let Some((_, right)) = text.split_once("```annotation\r\n") {
+        if let Some((left, _)) = right.split_once("\r\n```") {
+            return Some(serde_json::from_str(left));
+        }
+    }
+
+    None
+}
---
+++ src/main.rs
@@ -0,0 +1,214 @@
+use std::net::SocketAddr;
+
+use annotations::{extract_annotation, Annotation, GitHubStatus};
+use axum::{
+    http::{HeaderValue, StatusCode},
+    response::{IntoResponse, Response},
+    routing::{delete, get, post},
+    Json, Router,
+};
+use octocrab::{models, models::IssueState, params};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Read Personal Access Token (PAT)
+    let pat = std::fs::read_to_string("pat.txt")
+        .unwrap()
+        .trim()
+        .to_string();
+
+    octocrab::initialise(octocrab::Octocrab::builder().personal_token(pat.into())).unwrap();
+
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/recogito.min.css", get(css))
+        .route("/recogito.min.css.map", get(css_map))
+        .route("/recogito.min.js", get(js))
+        .route("/recogito.min.js.map", get(js_map))
+        .route("/favicon.ico", get(favicon))
+        .route("/annotations", get(get_annotations))
+        .route("/annotation", post(post_annotation))
+        .route("/annotation", delete(delete_annotation));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> impl IntoResponse {
+    let page = {
+        let index = std::fs::read_to_string("frontend/index.html").unwrap();
+        let document = std::fs::read_to_string("assets/draft-ietf-mls-protocol-17.txt").unwrap();
+
+        index.replace("{DOCUMENT}", &document)
+    };
+
+    Response::builder()
+        .header("Content-Type", "text/html")
+        .body(page)
+        .unwrap()
+}
+
+async fn css() -> impl IntoResponse {
+    Response::builder()
+        .header("Content-Type", "text/css")
+        .body(std::fs::read_to_string("frontend/recogito.min.css").unwrap())
+        .unwrap()
+}
+
+async fn css_map() -> impl IntoResponse {
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .body(std::fs::read_to_string("frontend/recogito.min.css.map").unwrap())
+        .unwrap()
+}
+
+async fn js() -> impl IntoResponse {
+    Response::builder()
+        .header("Content-Type", "text/javascript")
+        .body(std::fs::read_to_string("frontend/recogito.min.js").unwrap())
+        .unwrap()
+}
+
+async fn js_map() -> impl IntoResponse {
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .body(std::fs::read_to_string("frontend/recogito.min.js.map").unwrap())
+        .unwrap()
+}
+
+async fn favicon() -> impl IntoResponse {
+    let favicon = std::fs::read("frontend/assets/favicon.ico").unwrap();
+
+    let mut response = IntoResponse::into_response(favicon);
+    response
+        .headers_mut()
+        .insert("Content-Type", HeaderValue::from_static("image/x-icon"));
+
+    (StatusCode::OK, response)
+}
+
+async fn get_annotations() -> impl IntoResponse {
+    let octocrab = octocrab::instance();
+
+    let mut annotations = Vec::new();
+
+    let mut page = octocrab
+        .issues("duesee", "mls-validation")
+        .list()
+        .labels(&[String::from("validation")])
+        .state(params::State::All)
+        .per_page(50)
+        .send()
+        .await
+        .unwrap();
+
+    loop {
+        for issue in &page {
+            if let Some(body) = &issue.body {
+                if let Some(Ok(mut annotation)) = extract_annotation(body) {
+                    if issue.state == IssueState::Open {
+                        annotation.meta = Some(GitHubStatus::Open);
+                    } else {
+                        annotation.meta = Some(GitHubStatus::Closed);
+                    }
+
+                    annotations.push(annotation);
+                }
+            }
+        }
+        page = match octocrab
+            .get_page::<models::issues::Issue>(&page.next)
+            .await
+            .unwrap()
+        {
+            Some(next_page) => next_page,
+            None => break,
+        }
+    }
+
+    (StatusCode::CREATED, Json(annotations))
+}
+
+async fn post_annotation(Json(annotation): Json<Annotation>) -> impl IntoResponse {
+    let octocrab = octocrab::instance();
+
+    let body = format!(
+        "> {}\r\n\r\n\r\n---\r\n<details><summary>Annotation</summary>\r\n\r\n```annotation\r\n{}\r\n```\r\n</details>",
+        annotation
+            .text_quote_selector()
+            .unwrap_or("```\r\n<no quote>\r\n```".into()),
+        serde_json::to_string_pretty(&annotation).unwrap()
+    );
+
+    if let Some(issue_number) = find_issue(&annotation).await {
+        match octocrab
+            .issues("duesee", "mls-validation")
+            .update(issue_number)
+            .title(&format!("[Validation] {}", annotation.title()))
+            .body(&body)
+            .send()
+            .await
+        {
+            Ok(_) => StatusCode::ACCEPTED,
+            Err(_) => StatusCode::NOT_ACCEPTABLE,
+        }
+    } else {
+        match octocrab
+            .issues("duesee", "mls-validation")
+            .create(format!("[Validation] {}", annotation.title()))
+            .labels(Some(vec!["validation".to_string()]))
+            .body(body)
+            .send()
+            .await
+        {
+            Ok(_) => StatusCode::CREATED,
+            Err(_) => StatusCode::NOT_ACCEPTABLE,
+        }
+    }
+}
+
+async fn delete_annotation() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn find_issue(annotation: &Annotation) -> Option<u64> {
+    let octocrab = octocrab::instance();
+
+    let mut page = octocrab
+        .issues("duesee", "mls-validation")
+        .list()
+        .labels(&[String::from("validation")])
+        .state(params::State::All)
+        .per_page(50)
+        .send()
+        .await
+        .unwrap();
+
+    loop {
+        for issue in &page {
+            if let Some(body) = &issue.body {
+                if let Some(Ok(issue_annotation)) = extract_annotation(body) {
+                    if issue_annotation.id == annotation.id {
+                        return Some(issue.number);
+                    }
+                }
+            }
+        }
+        page = match octocrab
+            .get_page::<models::issues::Issue>(&page.next)
+            .await
+            .unwrap()
+        {
+            Some(next_page) => next_page,
+            None => break,
+        }
+    }
+
+    None
+}
